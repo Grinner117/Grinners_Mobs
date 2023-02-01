bf75aceab614318e5ae8afaaeaf28597f6e563eb
Grinner117 <tyler.jefferies117@gmail.com>
2023-02-01 19:11:44 +1100

    Mission Fail, Rollback to pre Armor implementation

--- src/main/java/net/grinner117/grinnersmobs/item/ModItems.java
+++ src/main/java/net/grinner117/grinnersmobs/item/ModItems.java
@@ -1,14 +1,10 @@
 package net.grinner117.grinnersmobs.item;
 
 import net.grinner117.grinnersmobs.GrinnersMobs;
-import net.grinner117.grinnersmobs.ModArmorMaterial.LightMonsterBoneArmorMaterial;
-import net.grinner117.grinnersmobs.armor.LightMonsterBoneArmor;
 import net.grinner117.grinnersmobs.entity.ModEntityTypes;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeSpawnEggItem;
@@ -34,24 +30,12 @@ public class ModItems {
                             .effect(() -> new MobEffectInstance(MobEffects.WEAKNESS, 100, 0), 0.5F)
                             .effect(() -> new MobEffectInstance(MobEffects.HARM, 30, 0), 0.5F)
                             .build())));
-        public static final RegistryObject<Item> MONSTERBONE = ITEMS.register("monsterbone",
+    public static final RegistryObject<Item> MONSTERBONE = ITEMS.register("monsterbone",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.GRINNERSTAB)));
     public static final RegistryObject<Item> MONSTERLEATHER = ITEMS.register("monsterleather",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.GRINNERSTAB)));
 
-    public static final RegistryObject<Item> LIGHTMONSTERBONE_HELMET = ITEMS.register("lightmonsterbone_helmet",
-            () -> new ArmorItem(LightMonsterBoneArmorMaterial.LIGHTMONSTERBONEARMOR, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeModeTab.GRINNERSTAB)));
-
-    public static final RegistryObject<Item> LIGHTMONSTERBONE_CHESTPLATE = ITEMS.register("lightmonsterbone_chestplate",
-            () -> new ArmorItem(LightMonsterBoneArmorMaterial.LIGHTMONSTERBONEARMOR, EquipmentSlot.CHEST, new Item.Properties().tab(ModCreativeModeTab.GRINNERSTAB)));
-
-    public static final RegistryObject<Item> LIGHTMONSTERBONE_LEGGINGS = ITEMS.register("lightmonsterbone_leggings",
-            () -> new ArmorItem(LightMonsterBoneArmorMaterial.LIGHTMONSTERBONEARMOR, EquipmentSlot.LEGS, new Item.Properties().tab(ModCreativeModeTab.GRINNERSTAB)));
-
-    public static final RegistryObject<Item> LIGHTMONSTERBONE_BOOTS = ITEMS.register("lightmonsterbone_boots",
-            () -> new ArmorItem(LightMonsterBoneArmorMaterial.LIGHTMONSTERBONEARMOR, EquipmentSlot.FEET, new Item.Properties().tab(ModCreativeModeTab.GRINNERSTAB)));
-
-    public static void register(IEventBus eventBus) {
+       public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
